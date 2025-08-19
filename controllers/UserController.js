7db2cc532d4d077e905713f78db7fcd2312d92de
Spa Pas <jboadi@spepas.com>
2025-08-19 23:45:45 +0200

    user id added to signup

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,118 +4,135 @@ const { REGISTRATION_STATUS, RESPONSE_CODES } = require("../helper/vars");
 const jwt = require("jsonwebtoken");
 
 exports.LOGIN = asynHandler(async (req, res, next) => {
-  let { email, password } = req.body;
+  try {
+    let { email, password } = req.body;
 
-  var loginUrl = process.env.DB_BASE_URL + "user/login";
+    var loginUrl = process.env.DB_BASE_URL + "user/login";
 
-  var requestData = {
-    email,
-    password,
-  };
-
-  console.log("================coming to make a request================");
-  let newJob = await UtilityHelper.makeHttpRequest(
-    "POST",
-    loginUrl,
-    requestData
-  );
-  console.log("================after request================");
-
-  if (!newJob) {
-    var resp = {
-      status: RESPONSE_CODES.FAILED,
-      message: "Failed to connect to services",
-    };
-    return UtilityHelper.sendResponse(res, 200, resp.message, resp);
-  }
-
-  if (newJob.status != RESPONSE_CODES.SUCCESS) {
-    return UtilityHelper.sendResponse(res, 200, newJob.message, newJob);
-  }
-
-  //JWT_TOKEN
-
-  if (newJob.data.verificationStatus != RESPONSE_CODES.SUCCESS) {
-    //user is not verified generate OTP verification
-    //TO - DO
-    //1. Call the OTPManager to generate otp and send to user
-    let phoneNumber = newJob.data.phoneNumber;
-    let email = newJob.data.email;
-
-    var otpUrl = process.env.OTP_SERVICES_URL + "create";
-    var otpReqObj = {
-      counterType: "MINUTE",
-      sendSms: 1,
-      phoneNumber: phoneNumber,
-      sendEmail: 1,
-      emailMessage: "",
-      smsMessage: "",
-      expiryCounter: 10,
-      User_ID: newJob.data.User_ID,
+    var requestData = {
+      email,
+      password,
     };
 
-    let otpReqResp = await UtilityHelper.makeHttpRequest(
+    console.log("================coming to make a request================");
+    let newJob = await UtilityHelper.makeHttpRequest(
       "POST",
-      otpUrl,
-      otpReqObj
+      loginUrl,
+      requestData
     );
+    console.log("================after request================");
 
-    if (!otpReqResp) {
+    if (!newJob) {
       var resp = {
         status: RESPONSE_CODES.FAILED,
-        message: "Failed to connect to otp services",
+        message: "Failed to connect to services",
       };
       return UtilityHelper.sendResponse(res, 200, resp.message, resp);
     }
 
-    if (otpReqResp.status != RESPONSE_CODES.SUCCESS) {
-      return UtilityHelper.sendResponse(
-        res,
-        200,
-        otpReqResp.message,
-        otpReqResp
-      );
+    if (newJob.status != RESPONSE_CODES.SUCCESS) {
+      return UtilityHelper.sendResponse(res, 200, newJob.message, newJob);
     }
 
-    var resetID = otpReqResp.data;
+    //JWT_TOKEN
+
+    if (newJob.data.verificationStatus != RESPONSE_CODES.SUCCESS) {
+      //user is not verified generate OTP verification
+      //TO - DO
+      //1. Call the OTPManager to generate otp and send to user
+      let phoneNumber = newJob.data.phoneNumber;
+      let email = newJob.data.email;
+
+      var otpUrl = process.env.OTP_SERVICES_URL + "create";
+      var otpReqObj = {
+        counterType: "MINUTE",
+        sendSms: 1,
+        phoneNumber: phoneNumber,
+        sendEmail: 1,
+        emailMessage: "",
+        smsMessage: "",
+        expiryCounter: 10,
+        User_ID: newJob.data.User_ID,
+      };
+
+      let otpReqResp = await UtilityHelper.makeHttpRequest(
+        "POST",
+        otpUrl,
+        otpReqObj
+      );
+
+      if (!otpReqResp) {
+        var resp = {
+          status: RESPONSE_CODES.FAILED,
+          message: "Failed to connect to otp services",
+        };
+        return UtilityHelper.sendResponse(res, 200, resp.message, resp);
+      }
+
+      if (otpReqResp.status != RESPONSE_CODES.SUCCESS) {
+        return UtilityHelper.sendResponse(
+          res,
+          200,
+          otpReqResp.message,
+          otpReqResp
+        );
+      }
+
+      var resetID = otpReqResp.data;
+
+      var resp = {
+        status: RESPONSE_CODES.PENDING_REVIEW,
+        message:
+          "User verification is pending. An OTP has been sent to the user for authentication",
+        data: resetID,
+      };
+      return UtilityHelper.sendResponse(res, 200, resp.message, resp);
+    }
+
+    var tokenDetails = {
+      User_ID: newJob.data.User_ID,
+    };
+
+    const token = jwt.sign(tokenDetails, process.env.JWT_TOKEN, {
+      expiresIn: "10h",
+    });
+    const refresh_token = jwt.sign(
+      tokenDetails,
+      process.env.JWT_TOKEN_REFRESH,
+      {
+        expiresIn: "20h",
+      }
+    );
+
+    delete newJob.data.id;
+    delete newJob.data.User_ID;
+    delete newJob.data.password;
+
+    var resData = {
+      token: token,
+      user: newJob.data,
+      refresh_token: refresh_token,
+    };
 
     var resp = {
-      status: RESPONSE_CODES.PENDING_REVIEW,
-      message:
-        "User verification is pending. An OTP has been sent to the user for authentication",
-      data: resetID,
+      status: RESPONSE_CODES.SUCCESS,
+      message: "Success",
+      data: resData,
     };
+
     return UtilityHelper.sendResponse(res, 200, resp.message, resp);
+  } catch (error) {
+    var respt = {
+      status: RESPONSE_CODES.FAILED,
+      message: "Failed to signup",
+    };
+    return UtilityHelper.sendResponse(
+      res,
+      error.response.status,
+      error.response.data.message,
+      respt
+    );
   }
-
-  var tokenDetails = {
-    User_ID: newJob.data.User_ID,
-  };
-
-  const token = jwt.sign(tokenDetails, process.env.JWT_TOKEN, {
-    expiresIn: "10h",
-  });
-  const refresh_token = jwt.sign(tokenDetails, process.env.JWT_TOKEN_REFRESH, {
-    expiresIn: "20h",
-  });
-
-  delete newJob.data.id;
-  delete newJob.data.User_ID;
-  delete newJob.data.password;
-
-  var resData = {
-    token: token,
-    user: newJob.data,
-    refresh_token: refresh_token,
-  };
-
-  var resp = {
-    status: RESPONSE_CODES.SUCCESS,
-    message: "Success",
-    data: resData,
-  };
-
-  return UtilityHelper.sendResponse(res, 200, resp.message, resp);
 });
 
 exports.ADMIN_LOGIN = asynHandler(async (req, res, next) => {
